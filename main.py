1c594b9bfa383d8ecb3004d5805e9a1371c2ef2f
Kai Yang <kfstorm@outlook.com>
2021-12-29 21:22:55 +0800

    Add logs

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
+import logging
 
 from parser import FogMap, _tile_x_y_to_lng_lat, BLOCK_BITMAP_SIZE
 import gpxpy
 import gpxpy.gpx
 
 BLOCK_WIDTH = 64
-assert BLOCK_WIDTH ** 2 == BLOCK_BITMAP_SIZE * 8
+assert BLOCK_WIDTH**2 == BLOCK_BITMAP_SIZE * 8
+
 
 def get_points(map):
     for _, tile in map.tile_map.items():
@@ -14,14 +16,26 @@ def get_points(map):
                 for y in range(BLOCK_WIDTH):
                     offset = y * BLOCK_WIDTH + x
                     if block.bitmap[offset // 8] & (1 << (7 - offset % 8)):
-                        yield _tile_x_y_to_lng_lat(tile.x + block.x / 128 + x / 128 / 64, tile.y + block.y / 128 + y / 128 / 64)
+                        yield _tile_x_y_to_lng_lat(
+                            tile.x + block.x / 128 + x / 128 / 64,
+                            tile.y + block.y / 128 + y / 128 / 64)
+
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
     assert len(sys.argv) > 2
-    gpx = gpxpy.gpx.GPX()
+
+    logging.info("Loading Fog of World data.")
     map = FogMap(sys.argv[1])
+    logging.info("Fog of World data loaded.")
+
+    gpx = gpxpy.gpx.GPX()
     for lng, lat in get_points(map):
         gpx.waypoints.append(gpxpy.gpx.GPXWaypoint(lat, lng))
 
+    logging.info("Waypoints gathered. Writting output file.")
     with open(sys.argv[2], "w", encoding="utf8") as f:
         f.write(gpx.to_xml())
